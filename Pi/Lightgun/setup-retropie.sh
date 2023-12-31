e66559f56d0d24c6a350f9bb870462d2cae19847
Widge (The real) <77170359+Widge-5@users.noreply.github.com>
2023-12-31 15:45:54 +0000

    Add files via upload

---
+++ Pi/Lightgun/setup-retropie.sh
@@ -0,0 +1,4 @@
+cp -v RetroPieScripts/Ports/*.sh /home/pi/RetroPie/roms/ports/
+cp -v Borders/RetroArchBorders/* /opt/retropie/emulators/retroarch/overlays/
+for f in /home/pi/RetroPie/roms/ports/SindenLightgun*; do chmod -v 777 $f; done
+for f in /home/pi/RetroPie/roms/ports/StopSinden*; do chmod -v 777 $f; done
