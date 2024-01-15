e088fe79ddb67864c0a4aff4cf20cf9fc1674a1b
Widge (The real) <77170359+Widge-5@users.noreply.github.com>
2024-01-15 20:03:30 +0000

    Add files via upload

---
+++ download.sh
@@ -0,0 +1,3 @@
+mkdir -p /home/$USER/RetroPie-Setup/ext/Widge-Extras/scriptmodules/supplementary
+wget -O /home/$USER/RetroPie-Setup/ext/Widge-Extras/scriptmodules/supplementary/sinden_lightgun.sh https://github.com/Widge-5/SindenLightgun/raw/main/sinden_lightgun.sh
+rm -f ./download.sh
