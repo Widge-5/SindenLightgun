5263faabf0050f086821f79674201bcd85d4ddb0
Widge (The real) <77170359+Widge-5@users.noreply.github.com>
2024-01-29 16:29:03 +0000

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,4 +2,5 @@
 if [ $USER == "root" ]; then USERNAME=$SUDO_USER; else USERNAME=$USER; fi
 mkdir -p /home/$USERNAME/RetroPie-Setup/ext/Widge-Extras/scriptmodules/supplementary
 wget -O /home/$USERNAME/RetroPie-Setup/ext/Widge-Extras/scriptmodules/supplementary/sinden_lightgun.sh https://github.com/Widge-5/SindenLightgun/raw/main/sinden_lightgun.sh
+chown -R $USERNAME:$USERNAME /home/$USERNAME/RetroPie-Setup/ext/Widge-Extras/
 rm -f ./download.sh
